8fe02b7c7458d97f77eb077fd8d580f675ba0992
Jxkeorton <jakeorton99@gmail.com>
2023-12-28 17:29:29 +0000

    contact page done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Layout from 'Layout'
 import { Img, Text } from "components";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    emailjs
+      .sendForm(
+        'service_y3gnaar',
+        'template_5fv1vj2',
+        form.current,
+        'e3dNwKxoG5Xuv3hqO'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log('message sent');
+          setIsSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      )
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
+  const resetForm = () => {
+    setIsSuccess(false);
+  };
 
   return (
     <Layout>
-      <div className='flex flex-row items-center justify-center mt-[63px] md:gap-5 gap-20'>
+      <div className='flex flex-row md:flex-col items-center justify-center mt-[63px] md:gap-5 gap-20'>
         {/* left side */}
-        <Img
-          className="max-h-[900px] h-auto w-[40%] object-cover"
-          src="images/img_motobase.png"
-          alt="motobase"
-        />
+        <div className='flex w-full h-full md:justify-center'>
+          <Img
+            className="max-h-[900px] h-auto w-[80%] object-cover"
+            src="images/img_motobase.png"
+            alt="motobase"
+          />
+        </div>
+        
 
         {/* right side */}
-        <div className='flex flex-col justify-center'>
+        <div className='flex flex-col justify-center md:items-center w-[80%]'>
           <Text
             className="sm:text-[19px] md:text-[21px] text-[23px]"
             size="txtPlayfairDisplayRomanSemiBold23"
@@ -31,17 +68,35 @@ const Contact = () => {
           </Text>
 
 
-          <div>
-            <input type="text" placeholder="Your email" className=' my-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-900' />
+          <div className=' flex flex-col md:items-center w-full'>
 
-            <button className="bg-white-A700 md:flex h-[37px] md:items-center md:justify-center m-auto rounded-[15px] w-[50%] border border-solid border-black-900 transition-all duration-300 ease-in-out hover:bg-yellow-500" >
-                <Text
-                  className="absolute md:flex h-max md:items-center justify-center m-auto text-black-900 text-center text-xl w-max"
-                  size="txtPlayfairDisplayRomanRegular20Black900"
-                >
-                  Submit
-                </Text>
-            </button>
+          <Text
+            className="sm:text-[19px] md:text-[21px] text-[23px]"
+            size="txtPlayfairDisplayRomanSemiBold23"
+          >
+            Email
+          </Text>
+            <form ref={form} onSubmit={sendEmail} className='flex flex-col md:items-center w-full'>
+              <input type="text" placeholder="Name" name="user_name" className=' my-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-900 w-[70%] md:w-full'/>
+              <input type="email" placeholder="Your email" name="user_email" className=' my-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-900 w-[70%] md:w-full' />
+              <textarea placeholder="Message" name="message"  rows={8} className='my-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-900 w-[70%] md:w-full ' />
+
+              <input 
+                type="submit" 
+                className="bg-white-A700 h-[37px] rounded-[15px] w-[50%] border-2 border-solid border-black-900 transition-all duration-300 ease-in-out hover:bg-yellow-500 font-bold" 
+                disabled={isSubmitting}
+                value={
+                  isSubmitting
+                    ? 'Sending...'
+                    : isSuccess
+                    ? 'Sent! Send Another?'
+                    : 'Send'
+                }
+                onClick={isSuccess ? resetForm : undefined}
+              />
+              
+            </form>
+            
           </div>
           
 
